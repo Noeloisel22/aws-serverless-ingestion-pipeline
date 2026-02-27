03d120f1b12aa9acb5c60b0249b76e825cf09bef
Noé LOISEL <nloisel@infitex.local>
2026-02-27 10:27:51 +0100

    refactor: extract hardcoded values to terraform variables
    - Create variables.tf for aws_region and bucket_name
    - Update s3.tf and provider.tf to use dynamic variables
    - Add *.tfvars to .gitignore to prevent leaking sensitive data

--- s3.tf
+++ s3.tf
@@ -1,3 +1,3 @@
 resource "aws_s3_bucket" "data_ingestion" {
-  bucket = "nloisel-2026-data-pipeline" # Doit être unique au MONDE
+  bucket = var.bucket_name
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable bucket_name {
+    type = string
+    description = "s3 bucket unique name"
+}
+
+variable provider_region {
+    type = string
+    description = "aws region of deployment"
+}
